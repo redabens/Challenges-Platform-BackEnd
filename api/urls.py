a77cdce518d1588802e6a9bd7a7381677169d139
MAHDI ALI <126531731+Alilo2005@users.noreply.github.com>
2024-12-10 00:34:39 +0000

    models created

--- api/urls.py
+++
@@ -1,9 +0,0 @@
-# urls.py
-from django.urls import path
-from .views import ChallengeList, SubmissionList, TeamList
-
-urlpatterns = [
-    path('challenges/', ChallengeList.as_view(), name='challenge-list'),
-    path('submissions/', SubmissionList.as_view(), name='submission-list'),
-    path('teams/', TeamList.as_view(), name='team-list'),
-]
